ac993438b3f83e790177b52de6b168fe03e53bbf
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-05 17:42:27 +1300

    build spinner and show/hide results

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults)
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide results
+  document.getElementById('results').style.display = 'none'
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block'
+
+  setTimeout(calculateResults, 2000)
+
+  e.preventDefault()
+})
 
 // Calculate results function
-function calculateResults(e){
+function calculateResults(){
   console.log('Calculating....')
 
   // UI Vars
@@ -29,16 +39,27 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2)
     totalPayment.value = (monthly * calculatedPayments).toFixed(2)
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
+
+    // Show results
+    document.getElementById('results').style.display = 'block'
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none'
   } else {
     console.log('Please check your fields')
     showError('Please check your fields')
   }
 
-  e.preventDefault()
 }
 
 // Show Error
 function showError(error){
+  // Hide results
+  document.getElementById('results').style.display = 'none'
+
+  // Hide loader
+  document.getElementById('loading').style.display = 'none'
+
   // Create a div
   const errorDiv = document.createElement('div')
 
